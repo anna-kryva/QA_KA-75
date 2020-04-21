e198d1d4566c5bbeb87a1fcfd89f6821281cbf64
VVRud <vl.val.rud@gmail.com>
2020-04-21 21:57:41 +0300

    Removed unused

--- Task2_CompareVersions/Utils/Functions.cs
+++ Task2_CompareVersions/Utils/Functions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Utils
+﻿namespace Utils
 {
     public static class Functions
     {
@@ -10,19 +8,13 @@ namespace Utils
             v2 = v2.Replace(".", string.Empty);
             if (v1.Length > v2.Length) v2 = v2.PadRight(v1.Length, '0');
             else if (v1.Length < v2.Length) v1 = v1.PadRight(v2.Length, '0');
-            
+
             int v1Numeric = int.Parse(v1);
             int v2Numeric = int.Parse(v2);
 
             if (v1Numeric > v2Numeric) return 1;
             if (v1Numeric < v2Numeric) return -1;
             return 0;
-        }
-
-        public static string Pad(string v, int length, char value)
-        {
-            for (int i = 0; i < length; i++) v += value;
-            return v;
         }
     }
 }
