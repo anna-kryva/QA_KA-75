f6cef3d547d623a8b7cd0dcfd11bea8be6446b7e
VVRud <vl.val.rud@gmail.com>
2020-04-21 20:13:03 +0300

    Lab Finished

---
+++ Task1_FILO/Stack/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        private T[] _array;
+
+        public Stack()
+        {
+            Count = 0;
+            _array = new T[10];
+        }
+
+        public int Count { get; private set; }
+
+        public T Peek()
+        {
+            return Count > 0 ? _array[Count] : default;
+        }
+
+        public T Pop()
+        {
+            var result = Peek();
+            if (Count > 0) Count -= 1;
+            return result;
+        }
+
+        public void Push(T item)
+        {
+            Count += 1;
+            if (_array.Length <= Count) Array.Resize(ref _array, Count * 2);
+            _array[Count] = item;
+        }
+    }
+}
