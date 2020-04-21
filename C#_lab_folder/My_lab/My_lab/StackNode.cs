dc99f5ab8e019cf6b0bd87c0be1871a1a17cd9dd
Alex Shevchuk <spichka.vin@gmail.com>
2020-04-21 21:15:13 +0300

    Reverse

---
+++ C#_lab_folder/My_lab/My_lab/StackNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_lab
+{
+    class StackNode<T>
+    {
+        public T Data { get; set; }
+        public StackNode<T> next { get; set; }
+        public StackNode(T Data)
+        {
+            this.Data = Data;
+        }
+    }
+}
