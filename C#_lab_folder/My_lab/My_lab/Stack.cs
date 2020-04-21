509ec5d32d80f0f7807315c88654061bdffb44ad
Alex Shevchuk <spichka.vin@gmail.com>
2020-04-21 17:08:32 +0300

    Coding part is finished

---
+++ C#_lab_folder/My_lab/My_lab/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_lab
+{
+    class Stack<T>
+    {
+        StackNode<T> head;
+        int size;
+
+        public Stack()
+        {
+            head = null;
+            size = 0;
+        }
+
+        public void Push (T el)
+        {
+            var newHead = new StackNode<T>(el);
+            newHead.next = head;
+            head = newHead;
+            size++;
+        }
+
+        public T Pop()
+        {
+            if(size != 0)
+            {
+                var item = head.Data;
+                head = head.next;
+                size--;
+                return item;
+            }
+            return (default(T));
+        }
+
+        public T Peek()
+        {
+            if( size != 0)
+            {
+                return head.Data;
+            }
+            return default(T);
+        }
+
+        public int Count()
+        {
+            return size;
+        }
+            
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,11 @@ The KA-75 group's repository for the QA course (KPI)
 
 **Branch name pattern:**  
 `firstName.LastName_lab`
+
+Variant 3
+
+	Task 1
+Create my stack (FILO) implementation with functions Count, Push, Pop, Peek.
+	
+	Task 2
+Write Ñ# CompareVersions() method, which takes as parameters 2 strings. These strings are product versions. Product version consists of unlimited versions and subversions. Pattern for version is numbers and dot delimiters. If first version is greater than second method returns 1, if equals 0, if less -1.
