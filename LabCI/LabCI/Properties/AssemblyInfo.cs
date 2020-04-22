1ab6ea3b1f3755e15e07020fbfde5f3e350c4ccd
Egor Zagny <egor.zagny@gmail.com>
2020-04-22 23:07:06 +0300

    add my lab

---
+++ LabCI/LabCI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LabCI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LabCI")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c0a6a777-e3cb-4b99-a0b6-a14af350cf02")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LabCI/LabCI/Task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCI
+{
+    public class Task2
+    {
+        public static int CompareVersions(string str1, string str2)
+        {
+            string[] s1 = str1.Split('.');
+            string[] s2 = str2.Split('.');
+
+            for(int i = 0; true; i++)
+            {
+                if (i < s1.Length && i < s2.Length)
+                {
+                    if (int.Parse(s1[i]) > int.Parse(s2[i])) return 1;
+                    if (int.Parse(s1[i]) < int.Parse(s2[i])) return -1;
+                }
+                else if (i < s1.Length && i == s2.Length)
+                {
+                    if (int.Parse(s1[i]) > 0) return 1;
+                    else return 0;
+                }
+                else if (i == s1.Length && i < s2.Length)
+                {
+                    if (int.Parse(s2[i]) > 0) return -1;
+                    else return 0;
+                }
+                else return 0;
+            }
+
+        }
+    }
+}
