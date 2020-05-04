90041cc892c36968bbb4fd15f23b84626e491c61
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2020-05-04 16:57:48 +0300

    Adding Tests and Page object

--- PageObjectSolution/PageObject/Class1.cs
+++ PageObjectSolution/PageObject/Class1.cs
@@ -1,12 +1,174 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Interactions;
+
 
 namespace PageObject
 {
-    public class Class1
+    public class Page
     {
+        private IWebDriver _driver;
+
+        public Page(IWebDriver driver)  
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+
+        [FindsBy(How = How.CssSelector, Using = ".about button")]
+        public IWebElement AppealButton;
+        [FindsBy(How = How.CssSelector, Using = "input[type='checkbox']")]
+        public IWebElement AgreeCheckbox;
+        [FindsBy(How = How.CssSelector, Using = ".buttons button")]
+        public IWebElement AcquaintedButton;
+        [FindsBy(How = How.CssSelector, Using = ".PrepareQuestionsPage button")]
+        public IWebElement ContinueButton;
+        [FindsBy(How = How.CssSelector, Using = "div[role='alert']")]
+        public IWebElement AlertBlock;
+        [FindsBy(How = How.CssSelector, Using = ".Item :first-child")]
+        public IWebElement FirstCheckbox;
+        [FindsBy(How = How.CssSelector, Using = ".qa > :last-child > :nth-child(2)")]
+        public IWebElement CheckboxFalse;
+        [FindsBy(How = How.CssSelector, Using = ".qa > :last-child > :nth-child(1)")]
+        public IWebElement CheckboxTrue;
+        [FindsBy(How = How.CssSelector, Using = ".Section1_2 :nth-child(2) button")]
+        public IWebElement HelpButton;
+        [FindsBy(How = How.CssSelector, Using = ".ModalBodyHelp input[name='Name']")]
+        public IWebElement MolalNameInput;
+        [FindsBy(How = How.CssSelector, Using = ".ModalBodyHelp input[name='Telephone']")]
+        public IWebElement MolalTelephoneInput;
+        [FindsBy(How = How.XPath, Using = "//*[@id='root']/div/div[2]/div/form/div/label[1]/input")]
+        public IWebElement NameInput;
+        [FindsBy(How = How.CssSelector, Using = "button[type='submit']")]
+        public IWebElement SubmitButton;
+        [FindsBy(How = How.XPath, Using = "//*[@id='root']/div/div[2]/div/div/form/button")]
+        public IWebElement NextButton;
+        [FindsBy(How = How.CssSelector, Using = ".b4 .carModel span")]
+        public IWebElement InputSpan;
+        [FindsBy(How = How.XPath, Using = "//*[@id='root']/div/div[2]/div/form/div/div[2]/div[2]/label[1]/div[2]/span")]
+        public IWebElement DatePicker;
+        [FindsBy(How = How.CssSelector, Using = "div[aria-label='day-2']")]
+        public IWebElement Day23Picker;
+        [FindsBy(How = How.XPath, Using = "//*[@id='root']/div/div[2]/div/form/div/div[2]/div[2]/label[1]/div[2]/div[1]/div/input")]
+        public IWebElement DateInput;
+
+
+        public Page GoTo(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+            return this;
+        }
+        public string Get_url()
+        {
+            return _driver.Url;
+        }
+        public Page Appeal()
+        {
+            AppealButton.Click();
+            return this;
+        }
+        public Page ToAgreeCheckbox()
+        {
+            AgreeCheckbox.Click();
+            return this;
+        }
+        public Page Acquainte()
+        {
+            AcquaintedButton.Click();
+            return this;
+        }
+        public Page Continue()
+        {
+            ContinueButton.Click();
+            return this;
+        }
+        public string GetAlertText()
+        {
+            string text = AlertBlock.Text;
+            return text;
+        }
+        public Page FirstCheckboxClick()
+        {
+            FirstCheckbox.Click();
+            return this;
+        }
+        public Page CheckboxTrueClick()
+        {
+            CheckboxTrue.Click();
+            return this;
+        }
+        public Page CheckboxFalseClick()
+        {
+            CheckboxFalse.Click();
+            return this;
+        }
+        public Page HelpButtonClick()
+        {
+            HelpButton.Click();
+            return this;
+        }
+        public Page MolalNameInputInsert(string name)
+        {
+            MolalNameInput.SendKeys(name);
+            return this;
+        }
+        public Page MolalTelephoneInputtInsert(string tel)
+        {
+            MolalTelephoneInput.SendKeys(tel);
+            return this;
+        }
+        public Page NameInputInsert(string name)
+        {
+            NameInput.SendKeys(name);
+            return this;
+        }
+        public Page SubmitButtonClick()
+        {
+            SubmitButton.Click();
+            return this;
+        }
+        public string GetNextButtonText()
+        {
+            string text = NextButton.Text;
+            return text;
+        }
+        public Page NextButtonClick()
+        {
+            NextButton.Click();
+            return this;
+        }
+        public Page InputSpanMove()
+        {
+            Actions action = new Actions(_driver);
+            action.MoveToElement(InputSpan).Perform();
+            return this;
+        }
+        public Page DatePickerClick()
+        {
+            DatePicker.Click();
+            return this;
+        }
+        public Page Day2PickerClick()
+        {
+            Day23Picker.Click();
+            return this;
+        }
+        public string GetDateInputValue()
+        {
+            string val = DateInput.GetAttribute("value");
+            return val;
+        }
+        public Page Wait200()
+        {
+            Thread.Sleep(200);
+            return this;
+        }
+        public Page Wait1000()
+        {
+            Thread.Sleep(1000);
+            return this;
+        }
     }
 }
--- PageObjectSolution/Tests/MainClass.cs
+++ PageObjectSolution/Tests/MainClass.cs
@@ -1,16 +1,130 @@
 ﻿using NUnit.Framework;
-using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using PageObject;
+using System.Security.Policy;
 
 namespace Tests
 {
     [TestFixture]
     public class MainClass
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        private Page page;
+        private string _url = "https://avtoshtraf.com/";
+        [SetUp]
+        public void TestInitialize()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            page = new Page(driver);
+        }
+        [TearDown]
+        public void TestCleanup()
+        {
+            driver.Close();
+        }
+        [Test]
+        public void Test1()
+        {
+            page.GoTo(_url).Appeal().ToAgreeCheckbox().Acquainte();
+        }
+        [Test]
+        public void Test2()
+        {
+            string expected = "Заповніть усі відповіді!";
+            page.GoTo(_url).Appeal().Wait1000().ToAgreeCheckbox().Acquainte().Continue();
+            string result = page.GetAlertText();
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Test3()
+        {
+            string expected_url = "https://avtoshtraf.com/declaration";
+            page.GoTo(_url).Appeal().Wait1000().ToAgreeCheckbox().Acquainte();
+            page.FirstCheckboxClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick();
+            page.Continue();
+            Assert.AreEqual(page.Get_url(), expected_url);
+        }
+        [Test]
+        public void Test4()
+        {
+            string name = "Въячеслав Украинцев";
+            string tel = "+8800 555 35 35";
+            string expected_url = "https://avtoshtraf.com/";
+            page.GoTo(_url).Wait1000().HelpButtonClick()
+                .MolalNameInputInsert(name)
+                .MolalTelephoneInputtInsert(tel);
+            Assert.AreEqual(page.Get_url(), expected_url);
+        }
+        [Test]
+        public void Test5()
+        {
+            string name = "Въячеслав Украинцев";
+            string expected = "Оплатити 50 UAH".ToLower();
+            page.GoTo(_url).Appeal().Wait1000().ToAgreeCheckbox().Acquainte();
+            page.FirstCheckboxClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick();
+            page.Continue().NameInputInsert(name).SubmitButtonClick().Wait1000();
+            string result = page.GetNextButtonText().ToLower();
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Test7()
+        {
+            string expected = "2020-05-02";
+            page.GoTo(_url).Appeal().Wait1000().ToAgreeCheckbox().Acquainte();
+            page.FirstCheckboxClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick();
+            page.Continue().DatePickerClick().Day2PickerClick();
+            string result = page.GetDateInputValue();
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Test6()
+        {
+            string expected = "Переписати зі свідоцтва про реєстрацію транспортного засобу";
+            page.GoTo(_url).Appeal().Wait1000().ToAgreeCheckbox().Acquainte();
+            page.FirstCheckboxClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick();
+            page.Continue().InputSpanMove();
+        }
+        public void Test8()
+        {
+            string name = "Майкл Джордан";
+            string expected = "https://www.liqpay.ua";
+            page.GoTo(_url).Appeal().Wait1000().ToAgreeCheckbox().Acquainte();
+            page.FirstCheckboxClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick()
+                .CheckboxFalseClick()
+                .CheckboxTrueClick();
+            page.Continue().NameInputInsert(name).SubmitButtonClick().NextButtonClick();
+            string result = page.Get_url().Substring(0, 21);
+            Assert.AreEqual(result, expected);
         }
     }
 }
