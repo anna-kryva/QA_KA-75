df4a86fc1add812d2853d6c8cf83233224b9c49a
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2020-05-04 12:51:39 +0300

    Creating carcass

---
+++ PageObjectSolution/Tests/MainClass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MainClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
