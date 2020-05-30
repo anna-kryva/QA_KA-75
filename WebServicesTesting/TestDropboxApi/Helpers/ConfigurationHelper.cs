78f4253c8a9ed82ffa274539d2526af5737caa40
Alex Shevchuk <spichka.vin@gmail.com>
2020-05-30 22:03:44 +0300

    Token battle

--- WebServicesTesting/TestDropboxApi/Helpers/ConfigurationHelper.cs
+++ WebServicesTesting/TestDropboxApi/Helpers/ConfigurationHelper.cs
@@ -11,7 +11,14 @@ namespace TestDropboxApi.Helpers
     {
         public static string ServiceUrl => ConfigurationManager.AppSettings["serviceUrl"];
         public static string ContentServiceUrl => ConfigurationManager.AppSettings["contentServiceUrl"];
-        public static string AuthorizationToken => ConfigurationManager.AppSettings["token"];
+        public static string AuthorizationToken()
+        {
+            var fPart = "sl.AbB25EfsdMwiDUAC0a0Wm5ZSNEVzf_";
+            var sPart = "y8lEFnWx_mdi7vgthX_4GrBETGS08Kaz - qoNN - ";
+            var tPart = "_nqM14wcLPcftNpwOLqETzAnViWi6XjSi_ - ";
+            var lPart = "MOMoOLvi31nFChjUtpaAUYd7YNmG3PTmN";
+            return fPart + sPart + tPart + lPart;
+        }
         public static string DefaultFilePath => ConfigurationManager.AppSettings["defaultFilePath"];
 
     }
