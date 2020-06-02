9e8a19a0ebb1790e932aeeaa834db76308b50ca0
Roman-Siratsky <romadep228@gmail.com>
2020-06-02 12:55:30 +0300

    Added Tests

---
+++ LabCI/LabCI/MyStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCI
+{
+    public class MyStack<T>
+    {
+        private T[] stack;
+        private int count = 0;
+        public int Count { get { return count; } }
+
+        public MyStack(){
+            stack = new T[10];
+        }
+
+        public MyStack<T> Push(T t)
+        {
+            if(stack.Length == count)
+            {
+                Array.Resize(ref stack, count + 10);
+            }
+            stack[count] = t;
+            count++;
+            return this;
+        }
+        public T Pop()
+        {
+            count--;
+            return stack[count];
+        }
+        public T Peek()
+        {
+            return stack[0];
+        }
+    }
+}
---
+++ LabCI/LabCI/Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LabCI
+{
+    [TestFixture]
+    public class Tests
+    {
+        //Task1
+        [Test]
+        public void TestMyStack1()
+        {
+            MyStack<int> stack = new MyStack<int>().Push(1).Push(2).Push(3);
+            Assert.AreEqual(stack.Pop(), 3);
+        }
+        [Test]
+        public void TestMyStack2()
+        {
+            MyStack<double> stack = new MyStack<double>().Push(1).Push(2.71).Push(3.14);
+            Assert.AreEqual(stack.Peek(), 1);
+        }
+        [Test]
+        public void TestMyStack3()
+        {
+            MyStack<string> stack = new MyStack<string>().Push("Gryffindor").Push("Hufflepuff").Push("Ravenclaw").Push("Slytherin");
+            Assert.AreEqual(stack.Count, 4);
+        }
+        //Task2
+        [Test]
+        public void Task2Test1()
+        {
+            Assert.AreEqual(Task2.CompareVersions("4.5.3", "4.4.2"), 1);
+        }
+        [Test]
+        public void Task2Test2()
+        {
+            Assert.AreEqual(Task2.CompareVersions("1.0", "1"), 0);
+        }
+        [Test]
+        public void Task2Test3()
+        {
+            Assert.AreEqual(Task2.CompareVersions("1.0.1", "1.1.0"), -1);
+        }
+    }
+}
