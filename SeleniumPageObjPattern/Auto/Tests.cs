8affaf998420bcc5e63847d414a5aafc96e6cde6
Roman-Siratsky <romadep228@gmail.com>
2020-06-02 12:50:51 +0300

    Selenium Page Object

---
+++ SeleniumPageObjPattern/Auto/Tests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObject;
+
+namespace Auto
+{
+    [TestFixture]
+    public class Tests_Main
+    {
+        private IWebDriver driver;
+        private Page PgObj;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            PgObj = new Page(driver);
+        }
+        [TearDown]
+        public void finish()
+        {
+            driver.Close();
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            string search = "перси джексон";
+
+            PgObj._gotopage("http://baskino.me/")._search()._fillinsearch(search)._find()._persi();
+            
+        }
+
+
+        [Test]
+        public void Test2()
+        {
+            string loginname = "roman@gmail.com";
+            string loginpassword = "12345678";
+
+            PgObj._gotopage("http://baskino.me/")._login()._fillinloginname(loginname)._fillinloginpassword(loginpassword)._entrence();
+            
+        }
+        
+
+        [Test]
+        public void Test3()
+        {
+            PgObj._gotopage("http://baskino.me/")._rating()._page2()._page3()._forward()._lastpage();
+        }
+      
+
+        [Test]
+        public void Test4()
+        {
+            PgObj._gotopage("http://baskino.me/")._next()._prev();
+        }
+       
+
+        [Test]
+        public void Test5()
+        {
+            PgObj._gotopage("http://baskino.me/")._criminal()._date();
+        }
+
+        [Test]
+        public void Test6()
+        {
+
+            string name = "меню";
+            string email = "romadep228@gmail.com";
+            string subject = "сдача лаб";
+            string message = "помогите сделать лабу";
+
+            PgObj._gotopage("http://baskino.me/")._help()._feedback()._fillinname(name)._fillinemail(email)._fillinsubject(subject)._fillinmessage(message)._send();
+        }
+
+        [Test]
+        public void Test7()
+        {
+            string lostname = "ghjgkj";
+
+            PgObj._gotopage("http://baskino.me/")._login()._forgotten()._lostname(lostname)._submit();
+        }
+
+
+        [Test]
+        public void Test8()
+        {
+            string organization = "kpi";
+            string abusename = "menu";
+            string abuseemail = "laba@ukr.net";
+            string abuselinks = "hfjdklsh";
+            string abusedoc = "hfkjdsh";
+            string abusetext = "jkfdjs";
+
+            PgObj._gotopage("http://baskino.me/")._report()._fillinorganization(organization)._fillinabusename(abusename)._fillinabuseemail(abuseemail)._fillinabuselinks(abuselinks)._fillinabusedoc(abusedoc)._fillinabusetext(abusetext)._sendreport();
+        }
+    }
+}
---
+++ SeleniumPageObjPattern/PageObject/Object.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject
+{
+    public class Page
+    {
+        private IWebDriver _driver;
+        
+        public Page(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+
+        [FindsBy(How = How.Id, Using = "story")]
+        public IWebElement Search;
+        [FindsBy(How = How.LinkText, Using = "Поиск")]
+        public IWebElement FoundButton;
+        [FindsBy(How = How.CssSelector, Using = ".shortpost:nth-child(4) img:nth-child(1)")]
+        public IWebElement Persi;
+
+        [FindsBy(How = How.Id, Using = "login_button")]
+        public IWebElement Login;
+        [FindsBy(How = How.Id, Using = "login_name")]
+        public IWebElement LoginName;
+        [FindsBy(How = How.Id, Using = "login_password")]
+        public IWebElement LoginPassword;
+        [FindsBy(How = How.Name, Using = "image")]
+        public IWebElement Entrence;
+
+        [FindsBy(How = How.LinkText, Using = "Рейтингу")]
+        public IWebElement Rating;
+        [FindsBy(How = How.CssSelector, Using = ".navigation > a:nth-child(3)")]
+        public IWebElement Page2;
+        [FindsBy(How = How.CssSelector, Using = ".navigation > a:nth-child(4)")]
+        public IWebElement Page3;
+        [FindsBy(How = How.LinkText, Using = "Вперед")]
+        public IWebElement Forward;
+        [FindsBy(How = How.LinkText, Using = "1985")]
+        public IWebElement LastPage;
+
+        [FindsBy(How = How.CssSelector, Using = ".next")]
+        public IWebElement Next;
+        [FindsBy(How = How.CssSelector, Using = ".prev")]
+        public IWebElement Prev;
+
+        [FindsBy(How = How.LinkText, Using = "Криминальные")]
+        public IWebElement Criminal;
+        [FindsBy(How = How.LinkText, Using = "Дате")]
+        public IWebElement Date;
+
+        [FindsBy(How = How.LinkText, Using = "Помощь")]
+        public IWebElement Help;
+        [FindsBy(How = How.CssSelector, Using = ".hfeedback")]
+        public IWebElement Feedback;
+        [FindsBy(How = How.Name, Using = "name")]
+        public IWebElement Name;
+        [FindsBy(How = How.Name, Using = "email")]
+        public IWebElement Email;
+        [FindsBy(How = How.Name, Using = "subject")]
+        public IWebElement Subject;
+        [FindsBy(How = How.Name, Using = "message")]
+        public IWebElement Message;
+        [FindsBy(How = How.CssSelector, Using = ".fbutton > span")]
+        public IWebElement Send;
+
+        [FindsBy(How = How.LinkText, Using = "Восстановить пароль")]
+        public IWebElement Forgotten;
+        [FindsBy(How = How.Name, Using = "lostname")]
+        public IWebElement Lostname;
+        [FindsBy(How = How.CssSelector, Using = ".fbutton > span")]
+        public IWebElement Submit;
+
+        [FindsBy(How = How.LinkText, Using = "Для правообладателей")]
+        public IWebElement Report;
+        [FindsBy(How = How.Id, Using = "abuse_organization")]
+        public IWebElement Organization;
+        [FindsBy(How = How.Id, Using = "abuse_name")]
+        public IWebElement Abusename;
+        [FindsBy(How = How.Id, Using = "abuse_email")]
+        public IWebElement Abuseemail;
+        [FindsBy(How = How.Id, Using = "abuse_links")]
+        public IWebElement Abuselinks;
+        [FindsBy(How = How.Id, Using = "abuse_document")]
+        public IWebElement Abusedoc;
+        [FindsBy(How = How.Id, Using = "abuse_text")]
+        public IWebElement Abusetext;
+        [FindsBy(How = How.Name, Using = "send_btn")]
+        public IWebElement SendReport;
+
+        public Page _lastpage()
+        {
+            LastPage.Click();
+            return this;
+        }
+        public Page _forward()
+        {
+            Forward.Click();
+            return this;
+        }
+        public Page _page3()
+        {
+            Page3.Click();
+            return this;
+        }
+        public Page _page2()
+        {
+            Page2.Click();
+            return this;
+        }
+        public Page _rating()
+        {
+           Rating.Click();
+            return this;
+        }
+        public Page _prev()
+        {
+            Prev.Click();
+            Prev.Click();
+            Prev.Click();
+            return this;
+        }
+        public Page _next()
+        {
+            Next.Click();
+            Next.Click();
+            Next.Click();
+            return this;
+        }
+        public Page _submit()
+        {
+            Submit.Click();
+            return this;
+        }
+        public Page _lostname(string key)
+        {
+            Lostname.SendKeys(key);
+            return this;
+        }
+        public Page _forgotten()
+        {
+            Forgotten.Click();
+            return this;
+        }
+        public Page _sendreport()
+        {
+            SendReport.Click();
+            return this;
+        }
+        public Page _fillinabusetext(string key)
+        {
+            Abusetext.SendKeys(key);
+            return this;
+        }
+        public Page _fillinabusedoc(string key)
+        {
+            Abusedoc.SendKeys(key);
+            return this;
+        }
+        public Page _fillinabuselinks(string key)
+        {
+            Abuselinks.SendKeys(key);
+            return this;
+        }
+        public Page _fillinabuseemail(string key)
+        {
+            Abuseemail.SendKeys(key);
+            return this;
+        }
+        public Page _fillinabusename(string key)
+        {
+            Abusename.SendKeys(key);
+            return this;
+        }
+        public Page _report()
+        {
+            Report.Click();
+            return this;
+        }
+        public Page _fillinorganization(string key)
+        {
+            Organization.SendKeys(key);
+            return this;
+        }
+
+       
+        public Page _send()
+        {
+            Send.Click();
+            return this;
+        }
+        public Page _fillinmessage(string key)
+        {
+            Message.SendKeys(key);
+            return this;
+        }
+        public Page _fillinsubject(string key)
+        {
+            Subject.SendKeys(key);
+            return this;
+        }
+        public Page _fillinemail(string key)
+        {
+            Email.SendKeys(key);
+            return this;
+        }
+        public Page _fillinname(string key)
+        {
+            Name.SendKeys(key);
+            return this;
+        }
+        public Page _feedback()
+        {
+            Feedback.Click();
+            return this;
+        }
+        public Page _help()
+        {
+            Help.Click();
+            return this;
+        }
+        public Page _criminal()
+        {
+            Criminal.Click();
+            return this;
+        }
+        public Page _date()
+        {
+            Date.Click();
+            return this;
+        }
+       
+        public Page _entrence()
+        {
+            Entrence.Click();
+            return this;
+        }
+        public Page _fillinloginpassword(string key)
+        {
+            LoginPassword.SendKeys(key);
+            return this;
+        }
+        public Page _fillinloginname(string key)
+        {
+            LoginName.SendKeys(key);
+            return this;
+        }
+        public Page _login()
+        {
+            Login.Click();
+            return this;
+        }
+        public Page _persi()
+        {
+            Persi.Click();
+            return this;
+        }
+        public Page _find()
+        {
+            FoundButton.Click();
+            return this;
+        }
+        public Page _search()
+        {
+            Search.Click();
+            return this;
+        }
+
+        public Page _fillinsearch(string key)
+        {
+            Search.SendKeys(key);
+            return this;
+        }
+
+        public Page _gotopage(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+            return this;   
+        }
+
+       
+
+    }
+}
