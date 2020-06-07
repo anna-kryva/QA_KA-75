9e5070e2cf9c68284ab9a905cf61188cb7e23bee
Pavlo Trofymov <pavlo.trofymov@logika.com.ua>
2020-06-07 22:02:22 +0300

    added SeleniumPO

---
+++ SeleniumPageObjPattern/Auto/Tests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using PageObject;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests
+{
+	[TestFixture]
+	public class Tests
+	{
+		private IWebDriver driver;
+		private Page page;
+
+		[SetUp]
+		public void StartBrowser()
+		{
+			var options = new ChromeOptions();
+			options.AddArgument("start-maximized");
+			driver = new ChromeDriver(options);
+			page = new Page(driver);
+		}
+
+		[TearDown]
+		public void close_Browser()
+		{
+			driver.Quit();
+		}
+
+		[TestCase("examplemail.com")]
+		public void InvalidEmail(string email)
+		{
+			page.GoToUrl("https://app.pipedrive.com/auth/login")
+				.FillInEmailLogin(email)
+				.ClickLogin()
+				.Wait(1000)
+				;
+			Assert.AreEqual("Please add a valid email address", page.GetEmailErrorMessageLogin());
+		}
+
+		[TestCase("@email.com")]
+		[TestCase("email.com@")]
+		public void InvalidEmailAdvanced(string email)
+		{
+			page.GoToUrl("https://app.pipedrive.com/auth/login").FillInEmailLogin(email).ClickLogin().Wait(1000);
+			Assert.AreEqual("Please add a valid email address", page.GetEmailErrorMessageLogin());
+		}
+
+		[TestCase("qwe@emailcom")]
+		public void UnregisteredEmail(string email)
+		{
+			page.GoToUrl("https://app.pipedrive.com/auth/login/forgot_password")
+				.FillInEmailForgotPassword(email)
+				.ClickGotNewPasswordButton()
+				.Wait(1000)
+				;
+			Assert.AreNotEqual("Check your inbox", page.GetCheckInBoxText());
+		}
+
+		[Test]
+		public void GoogleAuth()
+		{
+			page.GoToUrl("https://app.pipedrive.com/auth/login")
+				.ClickGoogleAuthButton()
+				.Wait(1000)
+				;
+			Assert.IsTrue(page.UrlContains("https://accounts.google.com/signin/oauth"));
+		}
+
+		[Test]
+		public void SSOAuth()
+		{
+			page.GoToUrl("https://app.pipedrive.com/auth/login")
+				.ClickSSOAuthButton()
+				.Wait(1000)
+				;
+			Assert.IsTrue(page.UrlContains("https://app.pipedrive.com/auth/sso"));
+		}
+
+		[Test]
+		public void TryItFreeButton()
+		{
+			page.GoToUrl("https://www.pipedrive.com/")
+				.ClickTryItFreeButton()
+				.Wait(1000)
+				;
+			Assert.IsTrue(page.SingInPopUpDisplayed());
+		}
+
+		[Test]
+		public void DocumentationButton()
+		{
+			page.GoToUrl("https://developers.pipedrive.com/")
+				.ClickDocsButton()
+				.Wait(1000)
+				;
+
+			Assert.IsTrue(page.UrlContains("https://pipedrive.readme.io/docs"));
+		}
+
+		[Test]
+		public void APIButton()
+		{
+			page.GoToUrl("https://developers.pipedrive.com/")
+				.ClickAPIButton()
+				.Wait(1000)
+				;
+
+			Assert.IsTrue(page.UrlContains("https://developers.pipedrive.com/docs/api/"));
+
+		}
+	}
+}
---
+++ SeleniumPageObjPattern/PageObject/Object.cs
@@ -0,0 +1,149 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+//using SeleniumExtras.PageObjects;
+
+namespace PageObject
+{
+	public class Page
+	{
+		private IWebDriver driver_;
+
+		public Page(IWebDriver driver)
+		{
+			driver_ = driver;
+			PageFactory.InitElements(driver, this);
+		}
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div/div[1]/div[2]/div[1]/div/form/div/div[1]/div[1]/input")]
+		public IWebElement EmailFieldLogin;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div/div[1]/div[2]/div[1]/div/form/div/div[1]/div[2]")]
+		public IWebElement EmailErrorFieldLogin;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div/div[1]/div[2]/div[1]/div/form/div/div[3]/button")]
+		public IWebElement LoginButton;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/form/div/div[1]/div[1]/input")]
+		public IWebElement EmailFieldForgotPassword;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/form/div/div[2]/button")]
+		public IWebElement GotNewAPasswordButton;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div/div[1]/div[2]/div[1]/div/h1")]
+		public IWebElement CheckInbox;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/a")]
+		public IWebElement GoogleAuthButton;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/a")]
+		public IWebElement SSOAuthButton;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div[1]/div/div[1]/div[2]/div[1]")]
+		public IWebElement TryItFreeButton;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div/div[8]/div[2]/div[1]")]
+		public IWebElement SignUpPopup;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div[1]/div[4]/div[1]/a")]
+		public IWebElement DocsButton;
+
+		[FindsBy(How = How.XPath, Using = "/html/body/div[1]/div[4]/div[2]/a")]
+		public IWebElement APIButton;
+
+		public Page GoToUrl(string url)
+		{
+			driver_.Navigate().GoToUrl(url);
+			return this;
+		}
+
+		public Page FillInEmailLogin(string email)
+		{
+			EmailFieldLogin.SendKeys(email);
+			return this;
+		}
+
+		public Page FillInEmailForgotPassword(string email)
+		{
+			EmailFieldForgotPassword.SendKeys(email);
+			return this;
+		}
+
+		public Page ClickLogin()
+		{
+			LoginButton.Click();
+			return this;
+		}
+
+		public Page ClickGotNewPasswordButton()
+		{
+			GotNewAPasswordButton.Click();
+			return this;
+		}
+
+		public Page ClickGoogleAuthButton()
+		{
+			GoogleAuthButton.Click();
+			return this;
+		}
+
+		public Page ClickSSOAuthButton()
+		{
+			SSOAuthButton.Click();
+			return this;
+		}
+
+		public Page ClickTryItFreeButton()
+		{
+			TryItFreeButton.Click();
+			return this;
+		}
+
+		public Page ClickDocsButton()
+		{
+			DocsButton.Click();
+			return this;
+		}
+
+		public Page ClickAPIButton()
+		{
+			APIButton.Click();
+			return this;
+		}
+
+		public string GetCurrentUrl()
+		{
+			return driver_.Url;
+		}
+
+		public string GetEmailErrorMessageLogin()
+		{
+			return EmailErrorFieldLogin.Text;
+		}
+
+		public string GetCheckInBoxText()
+		{
+			return CheckInbox.Text;
+		}
+
+		public bool UrlContains(string text)
+		{
+			return driver_.Url.Contains(text);
+		}
+
+		public bool SingInPopUpDisplayed()
+		{
+			return SignUpPopup.Displayed;
+		}
+
+		public Page Wait(int timeout = 3000)
+		{
+			System.Threading.Thread.Sleep(timeout);
+			return this;
+		}
+
+		public Page SetSnth()
+		{
+			return this;
+		}
+	}
+}
