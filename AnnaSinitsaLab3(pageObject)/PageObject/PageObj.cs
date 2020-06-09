a7e3f3b30db0135872118bbd806f7c86188b22de
Ann Sinitsa <annasinitsa20@gmail.com>
2020-06-10 00:34:51 +0300

    Lab3 + minor changes in lab2

---
+++ AnnaSinitsaLab3(pageObject)/PageObject/PageObj.cs
@@ -0,0 +1,123 @@
+﻿using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObjSol
+{
+    public class PageObj
+    {
+        private IWebDriver _driver;
+
+        public PageObj(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+
+        [FindsBy(How = How.XPath, Using = "/html/body/article/section[1]/div[2]/div[2]/div[1]/div/a")]
+        public IWebElement Contact_Sales_Button;
+        [FindsBy(How = How.XPath, Using = "//*[@id='owner[email]']")]
+        public IWebElement EmailInput;
+        [FindsBy(How = How.XPath, Using = "//*[@id='question']")]
+        public IWebElement QuestionInput;
+        [FindsBy(How = How.XPath, Using = "//*[@id='FirstName']")]
+        public IWebElement FirstNameInput;
+        [FindsBy(How = How.XPath, Using = "//*[@id='LastName']")]
+        public IWebElement LastNameInput;
+        [FindsBy(How = How.XPath, Using = "//*[@id='account[name]']")]
+        public IWebElement Company_nameInput;
+        [FindsBy(How = How.XPath, Using = "//*[@id='account[help_desk_size]']")]
+        public IWebElement Number_of_employeesInput;
+        [FindsBy(How = How.XPath, Using = "//*[@id='address[phone]']")]
+        public IWebElement Phone_numberInput;
+        [FindsBy(How = How.XPath, Using = "/html/body/div[2]/div/form/div[1]/div[8]/button")]
+        public IWebElement SubmitButton;
+
+
+
+
+        public PageObj GoTo(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+            return this;
+        }
+        public string Get_url()
+        {
+            return _driver.Url;
+        }
+        public PageObj Activate_item(string itemXPath)
+        {
+            Actions actions = new Actions(_driver);
+            IWebElement link = this.Get_link(itemXPath);
+            actions.MoveToElement(link).Perform();
+            return this;
+        }
+        public IWebElement Get_link(string itemXPath)
+        {
+            IWebElement link = _driver.FindElement(By.XPath(itemXPath));
+            return link;
+        }
+        public bool Is_dispalyed(string itemXPath)
+        {
+            bool isnt = this.Get_link(itemXPath).FindElement(By.XPath(itemXPath)).Displayed;
+            return isnt;
+        }
+        public PageObj Contact_Sales_Button_Click()
+        {
+            Contact_Sales_Button.Click();
+            return this;
+        }
+        public PageObj SetEmailInput(string Work_email)
+        {
+            EmailInput.SendKeys(Work_email);
+            return this;
+        }
+        public PageObj SetQuestionInput(string Message)
+        {
+            QuestionInput.SendKeys(Message);
+            return this;
+        }
+        public PageObj SetFirstNameInput(string First_name)
+        {
+            FirstNameInput.SendKeys(First_name);
+            return this;
+        }
+        public PageObj SetLastNameInput(string Last_name)
+        {
+            LastNameInput.SendKeys(Last_name);
+            return this;
+        }
+        public PageObj SetCompany_nameInput(string Company_name)
+        {
+            Company_nameInput.SendKeys(Company_name);
+            return this;
+        }
+        public PageObj SetNumber_of_employeesInput(string Number_of_employees)
+        {
+            Number_of_employeesInput.SendKeys(Number_of_employees);
+            return this;
+        }
+        public PageObj SetPhone_numberInput(string Phone_number)
+        {
+            Phone_numberInput.SendKeys(Phone_number);
+            return this;
+        }
+        public PageObj SubmitButton_Click()
+        {
+            SubmitButton.Click();
+            return this;
+        }
+        public PageObj Wait1000()
+        {
+            Thread.Sleep(1000);
+            return this;
+        }
+        public PageObj Wait10000()
+        {
+            Thread.Sleep(10000);
+            return this;
+        }
+    }
+}
---
+++ AnnaSinitsaLab3(pageObject)/PageObject/Tests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace PageObjSol
+{
+    class Tests
+    {
+        [TestFixture]
+        public class Selenium
+        {
+            private IWebDriver driver;
+            private PageObj page;
+            private string _url = "https://www.zendesk.com/contact/";
+
+            [SetUp]
+            public void StartBrowser()
+            {
+                var options = new ChromeOptions();
+                options.AddArgument("start-maximized");
+                driver = new ChromeDriver(options);
+                page = new PageObj(driver);
+            }
+            [TearDown]
+            public void TestCleanup()
+            {
+                driver.Close();
+            }
+            [Test]
+            public void TestLink()
+            {
+                page.GoTo(_url);
+            }
+
+            [TestCase("/html/body/header/div/div[2]/nav/ul[2]/li[1]/span", "/html/body/header/div/div[2]/nav/ul[2]/li[1]/div")]
+            [TestCase("/html/body/header/div/div[2]/nav/ul[2]/li[3]/span", "/html/body/header/div/div[2]/nav/ul[2]/li[3]/div")]
+            [TestCase("/html/body/header/div/div[2]/nav/ul[2]/li[6]/span", "/html/body/header/div/div[2]/nav/ul[2]/li[6]/div")]
+            public void DropDownItems_AfterHover_ShouldBeVisible(string itemXPath, string dropdownMenuXPath)
+            {
+                page.GoTo(_url).Wait1000().Activate_item(itemXPath).Wait1000();
+                bool is_displayed = page.Is_dispalyed(dropdownMenuXPath);
+                Assert.IsTrue(is_displayed, "The dropdown menu is expected to be visible");
+            }
+
+            [Test]
+            public void Contact_Sales_Popup_Must_Appear_After_Click()
+            {
+                page.GoTo(_url).Contact_Sales_Button_Click().Wait1000();
+                bool is_displayed = page.Is_dispalyed("/html/body/div[2]");
+                Assert.IsTrue(is_displayed, "The contact sales popup is expected to be visible");
+            }
+
+            [Test]
+            public void Go_to_the_help_center_Must_Redirect_to_Support_Page()
+            {
+                page.GoTo(_url).Contact_Sales_Button_Click().Wait1000();
+                String currentURL = page.Get_url();
+                currentURL.Contains("https://support.zendesk.com/");
+            }
+
+            [TestCase("example1234@mail.com", "some msg", "Tom", "Smith", "Company", "5000+", "+77077-777-77-77")]
+            public void Contsct_Us_Request_Is_sent(String Work_email, String Message, String First_name, String Last_name, String Company_name, String Number_of_employees, String Phone_number)
+            {
+                page.GoTo(_url).Contact_Sales_Button_Click().Wait1000();
+                page
+                    .SetEmailInput(Work_email)
+                    .SetQuestionInput(Message)
+                    .SetFirstNameInput(First_name)
+                    .SetLastNameInput(Last_name)
+                    .SetCompany_nameInput(Company_name)
+                    .SetNumber_of_employeesInput(Number_of_employees)
+                    .SetPhone_numberInput(Phone_number);
+                page.SubmitButton_Click();
+                page.Wait10000();
+                bool is_displayed = page.Is_dispalyed("/html/body/div[2]/div/form/div[2]");
+                Assert.IsTrue(is_displayed, "The 'Your request has been sent!' is expected to be visible");
+            }
+        }
+    }
+}
